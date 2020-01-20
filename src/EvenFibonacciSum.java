13241e4e2d778682a891ab4e17a67e3bbf38cb8f
Anniekobia <kobiaannunziata94@gmail.com>
2020-01-20 11:58:07 +0300

    Problem 2 complete: Sum of even fibonacci numbers

--- src/EvenFibonacciSum.java
+++ src/EvenFibonacciSum.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class EvenFibonacciSum {
 
@@ -12,33 +11,26 @@ public class EvenFibonacciSum {
     public static int EvenFibonacciSum(int first, int second, int n) {
         pairs.add(first);
         pairs.add(second);
-        System.out.println("Pairs " + pairs);
-        System.out.println("Checked " + checked);
         for (int number : pairs) {
             if (!checked.contains(number)) {
                 checked.add(number);
-                System.out.println(number);
                 if (number < n) {
                     if (number % 2 == 0) {
                         sum = sum + number;
                     }
-                    if (number == pairs.get(pairs.size()-1)) {
+                    if (number == pairs.get(pairs.size() - 1)) {
                         next = first + second;
-                        System.out.println("Next " + next);
                         pairs.clear();
                         EvenFibonacciSum(second, next, n);
                     }
                 }
             }
         }
-        System.out.println("Sum " + sum);
         return sum;
     }
 
     public static void main(String[] args) {
-//        Scanner in = new Scanner(System.in);
-//        System.out.println("Input the number of times,and the test cases");
-
-        EvenFibonacciSum(1, 2, 100);
+        sum = EvenFibonacciSum(1, 2, 100);
+        System.out.println("Sum: " + sum);
     }
 }
